0c4aba6bb6f313b62c301626e3757ddfcb1fb660
Julian Wollersberger <julian.derzocker@gmx.at>
2018-06-23 16:39:30 +0200

    Initial commit with .gitignore and module shared.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Unit Testing
+I want to do unit testing with JUnit 5.
+
+# Configure IntelliJ
+Most module files *.iml are on GitHub, 
+namely the ones with dependencies inside
+this project.
+
+The ones with computer specific paths not.
+This will be the libGDX client.
---
+++ shared/src/at/dropical/shared/example/ExampleClient.java
@@ -0,0 +1,15 @@
+package at.dropical.shared.example;
+// Created by julian on 23.06.18.
+
+/**
+ * TODO Description
+ */
+public class ExampleClient {
+
+    public static void main(String[] args) {
+        System.out.println("YAY :)");
+    }
+}
+
+/* I have bug in my code and they won't go,
+ * glitches in my code and they won't go. */
